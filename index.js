5917b018c58cd27985fd0f392dd0c371494e9f94
Andy Chan <andy.chan9930@gmail.com>
2021-10-12 16:17:20 -0400

    commit tree 99d186d90a29436d9ddac5cd2401197610b40de5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Andy";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-const shortGreeting = `Welcome, ${currentUser.charAt()}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
